84a5ff30170c4f778836ca917e22a8fcef50dc61
Vitor Matos <vamatos48@hotmail.com>
2020-01-26 11:19:08 -0300

    refatorando código - css

--- menu.php
+++ menu.php
@@ -18,36 +18,6 @@ $usuario = db_select_one($sql, array(
 
 $boasVindas = "Olá, {$usuario['nome']}!";
 ?>
-<style>
-    #menu{
-        width:100%;
-        height:60px;
-        display:flex;
-        flex-direction:row;
-        justify-content:space-between;
-        align-items:center;
-        position:fixed;
-        top:0;
-        box-shadow:#000 0px -30px 50px;
-        z-index:999;
-    }
-    #menu ul{
-        display:flex;
-        flex-direction:row;
-        justify-content:space-between;
-        align-items:center;
-        width:100%;
-        height:100%;
-        margin-top:0;
-        margin-bottom:0;
-        border-radius:10px 5px 15px 20px;
-    }
-    li{
-        list-style:none;
-        align-items:center;
-    }
-
-</style>
 <nav id="menu" class="alert-info">
     <div class="container">
         <ul>
